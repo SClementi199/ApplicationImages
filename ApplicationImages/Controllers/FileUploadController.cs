17e6c4ede224b0aa74e1c4d2f38825e8366271ea
SClementi199 <sergio.clementi@alten.it>
2024-05-22 12:27:05 +0200

    #Fix previously implementation

--- ApplicationImages/Controllers/FileUploadController.cs
+++ ApplicationImages/Controllers/FileUploadController.cs
@@ -37,15 +37,7 @@ namespace ApplicationImages.Controllers
         public async Task<IActionResult> GetImageById(int id)
         {
             var res = await _uploadService.GetByIdAsync(id);
-            byte[] bytes = Convert.FromBase64String(res.Files);
-
-            // Create a MemoryStream and write the byte array to it
-            using (MemoryStream ms = new MemoryStream(bytes))
-            {
-                // Create an image from the MemoryStream
-                Image image = Image.FromStream(ms);
-                //return image.Save("file.svg", System.Drawing.Imaging.ImageFormat.)
-            }
+            return Ok(res);
         }
 
         [HttpGet]
